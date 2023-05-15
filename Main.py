08088bffc920cd4cf8d064802649c2a7a1a23be1
Mattias <walde87@msn.com>
2023-05-15 14:28:23 +0200

    try/except på wordnet-delen

--- Main.py
+++ Main.py
@@ -5,4 +5,7 @@ except ImportError:
     subprocess.check_call(["pip", "install", "nltk"])
     import nltk
 
-
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet', quiet=True, raise_on_error=True)
