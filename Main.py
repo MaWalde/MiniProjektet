7f334c695734c78c8670d2dbfc647bd25727aeaa
Mattias <walde87@msn.com>
2023-05-16 10:47:27 +0200

    Skapat och testat grundfunktion för synonymer

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
 import nltk_setup
 from nltk.corpus import wordnet
 
+def listaSynonymer(ord):
+    synonymer = []
+    for syn in wordnet.synsets(ord):
+        for lemma in syn.lemmas():
+            synonymer.append(lemma.name())
+    return synonymer
+
+ordet = "sport"
+synonymer = listaSynonymer(ordet)
+print(f"{synonymer}")
