1164df41ba917979bc31bd8824e14827f7142b1c
Mattias <walde87@msn.com>
2023-05-16 08:40:15 +0200

    Flyttat installationerna till en separat fil som kallas på vid programstart.

---
+++ nltk_setup.py
@@ -0,0 +1,10 @@
+import subprocess
+try:
+    import nltk
+except ImportError:
+    subprocess.check_call(["pip", "install", "nltk"])
+    import nltk
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet', quiet=True, raise_on_error=True)
