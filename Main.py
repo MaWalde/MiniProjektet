fccc68f90604d541abe07b342d65ceccbdfedd3f
Mattias <walde87@msn.com>
2023-05-16 14:12:17 +0200

    Fönsterskiss

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import nltk_setup
 from nltk.corpus import wordnet
+import tkinter
 
-def listaSynonymer(ord):
+def getSynonyms(ord):
     synonymer = []
     for syn in wordnet.synsets(ord):
         for lemma in syn.lemmas():
@@ -15,7 +16,21 @@ def getDefinition(ord):
     else:
         return "Can not find a definition for the given word."
 
-ordet = "sport"
-synonymer = listaSynonymer(ordet)
-definitionen = getDefinition(ordet)
-print(f"{synonymer} {definitionen}")
+# ordet = "sport"
+# synonymer = listaSynonymer(ordet)
+# definitionen = getDefinition(ordet)
+# print(f"{synonymer} {definitionen}")
+
+fönster = tkinter.Tk()
+fönster.title("Lexicon")
+etikett = tkinter.Label(fönster, text="Enter a word:")
+etikett.pack()
+intext = tkinter.Entry(fönster)
+intext.pack()
+knapp = tkinter.Button(fönster, text="Get synonyms", command=getSynonyms)
+knapp.pack()
+knapp2 = tkinter.Button(fönster, text="Get definition", command=getDefinition)
+knapp2.pack()
+resultat = tkinter.Label(fönster, text=" ")
+resultat.pack()
+fönster.mainloop()
