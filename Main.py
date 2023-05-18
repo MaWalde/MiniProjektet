7b118306e915e96d8fc7f6281d42e9afd278bebf
Mattias <walde87@msn.com>
2023-05-18 10:39:22 +0200

    Korrigerat splash samt refaktorerat main-win till separat metod, ändrat funktioner så dom fungerar med den nya strukturen.

--- Main.py
+++ Main.py
@@ -3,51 +3,49 @@ from nltk.corpus import wordnet
 from tkinter import *
 
 def getSynonyms():
-    ord = intext.get()
+    ord = fönster.intext.get()
     synonymer = []
     for syn in wordnet.synsets(ord):
         for lemma in syn.lemmas():
             synonymer.append(lemma.name())
-    resultat.config(text="Synonyms: " + ", ".join(synonymer))
+    fönster.resultat.config(text="Synonyms: " + ", ".join(synonymer))
 
 def getDefinition():
-    ord = intext.get()
+    ord = fönster.intext.get()
     definition = wordnet.synsets(ord)
     if definition:
-        resultat.config(text="Definition: " + definition[0].definition())
+        fönster.resultat.config(text="Definition: " + definition[0].definition())
     else:
-        resultat.config(text="Can not find a definition for the given word.")
+        fönster.resultat.config(text="Can not find a definition for the given word.")
+
+def splash_screen_stäng(event=None):
+    splash_screen.destroy()
+    startWin()
 
 def startWin():
-    start = Toplevel(fönster)
-    start.geometry("200x200")
-    label = Label(start, text="Experiment")
-    label.pack()
-
-def splash_screen_stäng():
-    splash_screen.destroy()
+    global fönster
+    fönster = Tk()
+    fönster.title("Lexicon")
+    fönster.geometry("800x500")
+    etikett = Label(fönster, text="Enter a word:")
+    etikett.pack()
+    fönster.intext = Entry(fönster)
+    fönster.intext.pack()
+    knapp = Button(fönster, text="Get synonyms", command=getSynonyms)
+    knapp.pack()
+    knapp2 = Button(fönster, text="Get definition", command=getDefinition)
+    knapp2.pack()
+    fönster.resultat = Label(fönster, text=" ")
+    fönster.resultat.pack()
+    #fönster.mainloop()
 
 splash_screen = Tk()
 splash_screen.title("Logotype")
-splash_img = PhotoImage(file="LeAl-logotyp.png")
+splash_screen.geometry("800x500")
+splash_img = PhotoImage(file="LeAl_logotyp.png")
 splash_label = Label(splash_screen, image=splash_img)
 splash_label.pack()
-splash_screen.after(3000, splash_screen_stäng())
-splash_screen.mainloop
-
-fönster = Tk()
-fönster.title("Lexicon")
-fönster.geometry("500x300")
-etikett = Label(fönster, text="Enter a word:")
-etikett.pack()
-intext = Entry(fönster)
-intext.pack()
-knapp = Button(fönster, text="Get synonyms", command=getSynonyms)
-knapp.pack()
-knapp2 = Button(fönster, text="Get definition", command=getDefinition)
-knapp2.pack()
-resultat = Label(fönster, text=" ")
-resultat.pack()
-knapp2 = Button(fönster, text="Öppna", command=startWin)
-knapp2.place(x=75,y=50)
-fönster.mainloop()
+# splash_screen.after(3000, splash_screen_stäng)
+# splash_label.bind("<Button-1>", splash_screen_stäng)
+splash_screen.bind("<Button-1>", splash_screen_stäng)
+splash_screen.mainloop()
