3e148769f0f7c48c1f8a52996093d3b5a2009961
Mattias <walde87@msn.com>
2023-05-21 09:59:07 +0200

    Kommenterar bort allt med temat då jag inte får till det. Löser bugg med oönskat fönster.

--- Main.py
+++ Main.py
@@ -1,27 +1,26 @@
 import nltk_setup
 from nltk.corpus import wordnet
 from tkinter import *
-from tkinter import ttk
+# from tkinter import ttk
 
-dark_mode = {".": {"configure": {"background": "#2d2d2d", "foreground": "white",}},
-             "Tlabel":{"configure":{"foreground": "white",}},
-             "Tbutton":{"configure":{"background":"#3c3f41","foreground":"white",}},
-             "TEntry": {"configure": {"background": "#2d2d2d", "foreground": "white",
-                        "fieldbackground" : "#4d4d4d", "insertcolor": "white",
-                        "bordercolor" : "black", "lightcolor" : "#4d4d4d",
-                        "darkcolor" : "black",}},
-             "TCheckbutton": {"configure": {"foreground": "white", "indicatorbackground" : "white", 
-                              "indicatorforeground" : "black",}},
-             "TCombobox": {"configure": {"background": "#2d2d2d", "foreground": "white",
-                                         "fieldbackground" : "#4d4d4d", "insertcolor": "white",
-                                         "bordercolor" : "black", "lightcolor" : "#4d4d4d",
-                                         "darkcolor" : "black", "arrowcolor" : "white"},},
-             }
+# dark_mode = {".": {"configure": {"background": "#2d2d2d", "foreground": "white",}},
+#              "Tlabel":{"configure":{"foreground": "white",}},
+#              "Tbutton":{"configure":{"background":"#3c3f41","foreground":"white",}},
+#              "TEntry": {"configure": {"background": "#2d2d2d", "foreground": "white",
+#                         "fieldbackground" : "#4d4d4d", "insertcolor": "white",
+#                         "bordercolor" : "black", "lightcolor" : "#4d4d4d",
+#                         "darkcolor" : "black",}},
+#              "TCheckbutton": {"configure": {"foreground": "white", "indicatorbackground" : "white", 
+#                               "indicatorforeground" : "black",}},
+#              "TCombobox": {"configure": {"background": "#2d2d2d", "foreground": "white",
+#                                          "fieldbackground" : "#4d4d4d", "insertcolor": "white",
+#                                          "bordercolor" : "black", "lightcolor" : "#4d4d4d",
+#                                          "darkcolor" : "black", "arrowcolor" : "white"},},
+#              }
 
-tema = ttk.Style()
-tema.theme_create('dark', parent="clam", settings=dark_mode)
-tema.theme_use('dark')
-
+# tema = ttk.Style()
+# tema.theme_create('dark', parent="clam", settings=dark_mode)
+# tema.theme_use('dark')
 
 def getSynonyms():
     ord = fönster.intext.get()
@@ -46,33 +45,39 @@ def splash_screen_stäng(event=None):
 def startWin():
     global fönster
     global splash_img
-    fönster = Toplevel()
+    fönster = Tk()
     fönster.protocol("WM_DELETE_WINDOW", fönster.quit)
     fönster.title("Lexicon")
     fönster.geometry("800x500")
-    etikett = ttk.Label(fönster, text="Enter a word:")
+    #etikett = ttk.Label(fönster, text="Enter a word:")
+    etikett = Label(fönster, text="Enter a word:")
     etikett.pack()
-    fönster.intext = ttk.Entry(fönster)
+    #fönster.intext = ttk.Entry(fönster)
+    fönster.intext = Entry(fönster)
     fönster.intext.pack()
-    knapp = ttk.Button(fönster, text="Get synonyms", command=getSynonyms)
+    #knapp = ttk.Button(fönster, text="Get synonyms", command=getSynonyms)
+    knapp = Button(fönster, text="Get synonyms", command=getSynonyms)
     knapp.pack()
-    knapp2 = ttk.Button(fönster, text="Get definition", command=getDefinition)
+    #knapp2 = ttk.Button(fönster, text="Get definition", command=getDefinition)
+    knapp2 = Button(fönster, text="Get definition", command=getDefinition)
     knapp2.pack()
-    fönster.resultat = ttk.Label(fönster, text=" ")
+    #fönster.resultat = ttk.Label(fönster, text=" ")
+    fönster.resultat = Label(fönster, text=" ")
     fönster.resultat.pack()
     #fönster.mainloop()
 
-splash_screen = Toplevel()
+splash_screen = Tk()
 splash_screen.title("Logotype")
 splash_screen.geometry("961x615")
-style = ttk.Style()
-style.configure("TSplashScreen.TLabel", background="#2d2d2d")
-style.configure("TSplashScreen.TLabel.image", background="#2d2d2d")
+# style = ttk.Style()
+# style.configure("TSplashScreen.TLabel", background="#2d2d2d")
+# style.configure("TSplashScreen.TLabel.image", background="#2d2d2d")
 splash_img = PhotoImage(file="LeAl_logotyp.png", master=splash_screen)
-splash_label = ttk.Label(splash_screen, image=splash_img, style="TSplashScreen.TLabel",)
+#splash_label = ttk.Label(splash_screen, image=splash_img, style="TSplashScreen.TLabel",)
+splash_label = Label(splash_screen, image=splash_img)
 splash_label.image = splash_img
 splash_label.pack()
-# splash_screen.after(3000, splash_screen_stäng)
+# splash_screen.after(3000, splash_screen_stäng)    Timer för splashscreen
 splash_label.bind("<Button-1>", splash_screen_stäng)
-#splash_screen.bind("<Button-1>", splash_screen_stäng)
+splash_screen.bind("<Button-1>", splash_screen_stäng)
 splash_screen.mainloop()
