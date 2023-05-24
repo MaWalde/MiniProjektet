ee52efcc562049006599a31bc09e8bdaacc35d1d
Mattias <walde87@msn.com>
2023-05-24 09:19:41 +0200

    Ändrat fäger, autostäng splash2 samt en border på botten

--- Main.py
+++ Main.py
@@ -76,6 +76,7 @@ def getWordClass():
 
 def showResFrame():
     bd_frame.grid()
+    bot_frame.grid()
 
 def splash_2(event=None):
     global titelbild
@@ -86,16 +87,17 @@ def splash_2(event=None):
     titel_label = Label(titelbild, image=titel_img)
     titel_label.image = titel_img
     titel_label.pack()
+    splash_screen.destroy()
     titelbild.after(4000, splash_screen_stäng)    #Timer för splashscreen
 
 def splash_screen_stäng(event=None):
-    splash_screen.destroy()
     titelbild.destroy()
     startWin()
 
 def startWin():
     global fönster
     global bd_frame
+    global bot_frame
     fönster = Tk()
     fönster.title("Mattias fantasticly spectacular english encyclopedia")
     fönster.maxsize(800,500)
@@ -104,30 +106,33 @@ def startWin():
     l_frame.grid(row=1,column=0, padx=20,pady=20)
     etikett = Label(l_frame, text="Enter a word:")
     fet_font = font.Font(weight="bold")
-    etikett.config(fg="blue", bg="skyblue",font=fet_font)
+    etikett.config(fg="#426cb6", bg="white",font=fet_font)
     etikett.grid(row=1,column=1,padx=10,pady=5)
     fönster.intext = Entry(l_frame)
     fönster.intext.config(font=fet_font)
     fönster.intext.grid(row=1,column=3,padx=10,pady=5)
     knapp = Button(l_frame, text="Get synonyms", command= getSynonyms)
     knapp_font = font.Font(weight="bold", size=11)
-    knapp.config(bg="skyblue", fg="blue", font=knapp_font, activebackground="gold")
+    knapp.config(bg="skyblue", fg="#426cb6", font=knapp_font, activebackground="white")
     knapp.grid(row=3,column=1,padx=10,pady=10)
     knapp2 = Button(l_frame, text="Get definition", command= getDefinition)
-    knapp2.config(bg="skyblue", fg="blue", font=knapp_font, activebackground="gold")
+    knapp2.config(bg="skyblue", fg="#426cb6", font=knapp_font, activebackground="white")
     knapp2.grid(row=3,column=3,padx=10,pady=10)
     knapp3 = Button(l_frame, text="Get autonyms", command= getAutonyms)
-    knapp3.config(bg="skyblue", fg="blue", font=knapp_font, activebackground="gold")
+    knapp3.config(bg="skyblue", fg="#426cb6", font=knapp_font, activebackground="white")
     knapp3.grid(row=4,column=1,padx=10,pady=10)
     knapp4 = Button(l_frame, text="Get class", command= getWordClass)
-    knapp4.config(bg="skyblue", fg="blue", font=knapp_font, activebackground="gold")
+    knapp4.config(bg="skyblue", fg="#426cb6", font=knapp_font, activebackground="white")
     knapp4.grid(row=4,column=3,padx=10,pady=10)
     bd_frame = Frame(fönster,width=180,height=250, bg="white",bd=5)
     bd_frame.grid(row=5,column=0,padx=10,pady=5)
+    bot_frame = Frame(fönster, width=0, height=20, bg="skyblue")
+    bot_frame.grid(row=7,column=0)
     fönster.resultat = Label(bd_frame, text=" ", wraplength=300, justify="left")
-    fönster.resultat.config(bg="skyblue", fg="blue", font=knapp_font)
+    fönster.resultat.config(bg="white", fg="#426cb6", font=knapp_font)
     fönster.resultat.grid(row=1,column=0,padx=10,pady=10)
     bd_frame.grid_forget()
+    bot_frame.grid_forget()
     #fönster.mainloop()
 
 splash_screen = Tk()
